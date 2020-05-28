6c172c1bf3134033c656b752b3075accb92e7bca
Sandeep Sharma <sandy335582@gmail.com>
2020-05-28 20:10:22 +0530

    used date-picker react lib

--- components/Filter.js
+++ components/Filter.js
@@ -1,70 +1,59 @@
 import React, { useState, useEffect, useContext } from 'react';
-import Error from '../components/Error/Error';
 import { EpisodeContext } from '../context/EpisodeContext';
-
+import DayPickerInput from 'react-day-picker/DayPickerInput';
 const Filter = () => {
   const [dates, setDates] = useState({
-    from: '2013-12-02',
-    to: '2020-05-28',
+    from: new Date('2013-12-02'),
+    to: new Date(),
   });
 
-  const [isValidDate, setDateValidity] = useState(true);
   const { setFilterDates } = useContext(EpisodeContext);
 
-  const setDate = (event) => {
+  function OverlayComponent({ children, ...props }) {
+    return <div {...props}>{children}</div>;
+  }
+  const setDate = (name, date) => {
+    console.log(name);
+    console.log(date);
     setDates({
       ...dates,
-      [event.target.name]: event.target.value,
+      [name]: date,
     });
   };
 
   useEffect(() => {
-    const from = Date.parse(dates.from + 'T00:00:00');
-    const to = Date.parse(dates.to + 'T00:00:00');
-    validateDates(from, to);
-    if (isValidDate) {
-      setFilterDates({ from, to });
-    }
+    const { from, to } = dates;
+    setFilterDates({ from, to });
   }, [dates]);
-
-  const validateDates = (from, to) => {
-    if (isNaN(from) || isNaN(to) || from > to) setDateValidity(false);
-    else return setDateValidity(true);
-  };
 
   return (
     <div className="block">
       <p className="subtitle">Air date Between</p>
       <div className="columns ">
         <div className="column is-4">
-          <div className="field">
-            <div className="control">
-              <input
-                name="from"
-                type="date"
-                value={dates.from}
-                className="input"
-                onChange={setDate}
-              />
-            </div>
-          </div>
+          <DayPickerInput
+            overlayComponent={OverlayComponent}
+            value={dates.from}
+            dayPickerProps={{
+              disabledDays: [{ after: dates.to }, { daysOfWeek: [2] }],
+              todayButton: 'Go to Today',
+            }}
+            onDayChange={(date) => setDate('from', date)}
+          />
         </div>
 
         <div className="column is-4">
-          <div className="field">
-            <div className="control">
-              <input
-                name="to"
-                type="date"
-                className="input"
-                value={dates.to}
-                onChange={setDate}
-              />
-            </div>
-          </div>
+          <DayPickerInput
+            overlayComponent={OverlayComponent}
+            dayPickerProps={{
+              disabledDays: [{ before: dates.from }, { daysOfWeek: [2] }],
+              todayButton: 'Go to Today',
+            }}
+            value={dates.to}
+            onDayChange={(date) => setDate('to', date)}
+          />
         </div>
       </div>
-      {!isValidDate && <Error errorMessage="Enter valid dates" />}
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import '../styles/styles.scss';
+import 'react-day-picker/lib/style.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
